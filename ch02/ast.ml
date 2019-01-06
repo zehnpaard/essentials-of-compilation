88e18ab7e0cf95d8a4f393d33a98780ab162e648
zehnpaard <zehnpaard@googlemail.com>
2019-01-07 02:36:08 +0900

    Copy R0 as base for R1

---
+++ ch02/ast.ml
@@ -0,0 +1,7 @@
+type t =
+  | Int of int
+  | Read
+  | Neg of t
+  | Add of t * t
+
+type prog = Program of t
---
+++ ch02/interpreter.ml
@@ -0,0 +1,7 @@
+let rec interpret_exp = function
+  | Ast.Int _ as e -> e
+  | Ast.Read -> int_of_string (read_line ())
+  | Ast.Neg e -> - (interpret_exp e)
+  | Ast.Add (e1, e2) -> (interpret_exp e1) + (interpret_exp e2)
+
+let interpret_r0 = function Ast.Program e -> interpret_exp e
