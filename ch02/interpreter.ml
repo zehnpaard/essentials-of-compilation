cd65668f3045ca72b0e9f1681e5b5238f84fcbdb
zehnpaard <zehnpaard@googlemail.com>
2019-01-07 14:04:00 +0900

    Update interpreter with info and env

--- ch02/interpreter.ml
+++ ch02/interpreter.ml
@@ -1,7 +1,7 @@
-let rec interpret_exp = function
+let rec interpret_exp env = function
   | Ast.Int _ as e -> e
   | Ast.Read -> int_of_string (read_line ())
-  | Ast.Neg e -> - (interpret_exp e)
-  | Ast.Add (e1, e2) -> (interpret_exp e1) + (interpret_exp e2)
+  | Ast.Neg e -> - (interpret_exp env e)
+  | Ast.Add (e1, e2) -> (interpret_exp env e1) + (interpret_exp env e2)
 
-let interpret_r0 = function Ast.Program e -> interpret_exp e
+let interpret_r0 = function Ast.Program (_, e) -> interpret_exp [] e
