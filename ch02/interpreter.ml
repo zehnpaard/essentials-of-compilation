c9d5fb20741c19f44f53a3b374600a907f5706f5
zehnpaard <zehnpaard@googlemail.com>
2019-01-07 14:04:45 +0900

    Add env lookup and interpreter for Var

--- ch02/interpreter.ml
+++ ch02/interpreter.ml
@@ -1,7 +1,12 @@
+let rec lookup env x = match env with
+  | [] -> failwith "Variable " ^ x ^ " not bound"
+  | (y, v) :: env' -> if x = y then v else lookup env' x
+
 let rec interpret_exp env = function
   | Ast.Int _ as e -> e
   | Ast.Read -> int_of_string (read_line ())
   | Ast.Neg e -> - (interpret_exp env e)
   | Ast.Add (e1, e2) -> (interpret_exp env e1) + (interpret_exp env e2)
+  | Ast.Var x -> lookup env x
 
 let interpret_r0 = function Ast.Program (_, e) -> interpret_exp [] e
