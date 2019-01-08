b6c899a88018d48b6032b65e5bbb5ffd06ba45b5
zehnpaard <zehnpaard@googlemail.com>
2019-01-08 21:52:07 +0900

    Add C0 language AST

---
+++ ch02/czero.ml
@@ -0,0 +1,18 @@
+type arg =
+  | Int of int
+  | Var of string
+
+type exp =
+  | Atom of arg
+  | Read
+  | Neg of arg
+  | Add of arg * arg
+
+type stmt =
+  | Assign of arg * exp
+
+type tail =
+  | Return of exp
+  | Seq of stmt * tail
+
+type program = Program of info * (string * tail) list
