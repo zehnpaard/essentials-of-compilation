a3d593422523a27a2dbfdd7722da581c3f94e771
zehnpaard <zehnpaard@googlemail.com>
2019-01-09 22:11:23 +0900

    Add main.ml and dune config to test existing modules

---
+++ ch02/main.ml
@@ -0,0 +1,14 @@
+open Ast
+
+let x = Let ("x", (Int 5), (Add (Let ("x", (Int 10), (Var "x")), Var "x")))
+let y = Uniquify.uniquify [] x
+let z = Decomplex.decomplex_exp y
+let () = 
+  begin
+    print_endline (string_of_t x);
+    print_endline (string_of_int (Interpreter.interpret_exp [] x));
+    print_endline (string_of_t y);
+    print_endline (string_of_int (Interpreter.interpret_exp [] y));
+    print_endline (string_of_t z);
+    print_endline (string_of_int (Interpreter.interpret_exp [] z));
+  end
