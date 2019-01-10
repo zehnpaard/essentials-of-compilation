fb898f23fcb1f2d7c1eb50257e3ea0bd83856633
zehnpaard <zehnpaard@googlemail.com>
2019-01-11 00:59:22 +0900

    Add use of uncover in main

--- ch02/main.ml
+++ ch02/main.ml
@@ -4,6 +4,13 @@ let x = Let ("x", (Int 5), (Add (Let ("x", (Int 10), (Var "x")), Var "x")))
 let y = Uniquify.uniquify [] x
 let z = Decomplex.decomplex_exp y
 let a = Explicate.explicate_tail z
+let Czero.Program (info, _) = Uncover.uncover_locals (Czero.Program ([], [("start", a)]))
+
+let rec string_of_vars = function
+  | [] -> ""
+  | [s] -> s
+  | s :: xs -> s ^ ", " ^ (string_of_vars xs)
+
 let () = 
   begin
     print_endline (string_of_t x);
@@ -13,4 +20,5 @@ let () =
     print_endline (string_of_t z);
     print_endline (string_of_int (Interpreter.interpret_exp [] z));
     print_endline (Czero.string_of_tail a);
+    print_endline (string_of_vars info);
   end
