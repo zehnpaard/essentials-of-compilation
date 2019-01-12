2f4b9f4ed030ba194769204692f2b47728aaa544
zehnpaard <zehnpaard@googlemail.com>
2019-01-13 02:16:18 +0900

    Add string_of for registers

--- ch02/asm.ml
+++ ch02/asm.ml
@@ -38,10 +38,28 @@ type block =
 type prog =
   | Program of string * (string * block) list
 
+let string_of_reg = function
+  | Rsp -> "%rsp"
+  | Rbp -> "%rbp"
+  | Rax -> "%rax"
+  | Rbx -> "%rbx"
+  | Rcx -> "%rcx"
+  | Rdx -> "%rdx"
+  | Rsi -> "%rsi"
+  | Rdi -> "%rdi"
+  | R8 -> "%r8"
+  | R9 -> "%r9"
+  | R10 -> "%r10"
+  | R11 -> "%r11"
+  | R12 -> "%r12"
+  | R13 -> "%r13"
+  | R14 -> "%r14"
+  | R15 -> "%r15"
+
 let string_of_arg = function
   | Int n -> string_of_int n
-  | Reg _ -> "rax" (* fix later *)
-  | Deref (_, _) -> failwith "NotImplemented"
+  | Reg r -> string_of_reg r
+  | Deref (i, r) -> string_of_int i ^ "(" ^ string_of_reg r ^ ")"
   | Var s -> s
 
 let string_of_instr = function
