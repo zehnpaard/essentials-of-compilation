da14b3f99a31e135d27b547dafa3339c345a24e5
zehnpaard <zehnpaard@googlemail.com>
2019-01-15 23:54:02 +0900

    Fix function orders for uncover_live

--- ch03/uncover_live.ml
+++ ch03/uncover_live.ml
@@ -1,5 +1,19 @@
 open Asm
 
-let rec f rinstrs live =
+let g instr live = match instr with
+  | Addq (a1, a2) | Subq (a1, a2) | Movq (a1, a2) ->
+    (match a1, a2 with
+     | Var v1, Var v2 when v1 = v2 -> live
+     | Var v1, Var v2 -> v1 :: List.filter ((=) v2) live
+     | Var v, _ -> v :: live
+     | _, Var v -> List.filter ((=) v) live
+     | _, _ -> live)
+  | Pushq (Var v) -> v :: live
+  | Popq (Var v) -> List.filter ((=) v) live
+  | Pushq _ | Popq _ | Negq _ | Callq _ | Retq -> live
 
-let uncover instrs 
+let f lives rinstrs =
+  let f' lives instr = g instr (List.hd lives) :: lives in
+  List.fold_left f' lives rinstrs
+
+let uncover instrs = f [[]] (List.rev instrs)
