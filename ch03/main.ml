b00eb7787bee6b205efd4a4f652e1fbd1be88e66
zehnpaard <zehnpaard@googlemail.com>
2019-01-16 11:44:41 +0900

    Use uncover_live from main

--- ch03/main.ml
+++ ch03/main.ml
@@ -8,8 +8,9 @@ let czp1 = Explicate.f astp3
 let czp2 = Uncover.uncover_locals czp1
 let Czero.Program (info, _) = czp2
 let asmp1 = Select_instructions.f czp2
-let asmp2 = Assign_home.f asmp1
-let asmp3 = Patch_instructions.f asmp2
+let asmp2 = Uncover_live.f asmp1
+let asmp3 = Assign_home.f asmp2
+let asmp4 = Patch_instructions.f asmp3
 
 let () = 
   begin
@@ -21,4 +22,5 @@ let () =
     print_endline (Asm.string_of_prog asmp1);
     print_endline (Asm.string_of_prog asmp2);
     print_endline (Asm.string_of_prog asmp3);
+    print_endline (Asm.string_of_prog asmp4);
   end
