147d5305df92d80c42aef6851e8a71b8e1f0639a
zehnpaard <zehnpaard@googlemail.com>
2019-01-16 11:44:20 +0900

    Add function working on Asm.prog to uncover_live

--- ch03/uncover_live.ml
+++ ch03/uncover_live.ml
@@ -12,8 +12,14 @@ let g instr live = match instr with
   | Popq (Var v) -> List.filter ((=) v) live
   | Pushq _ | Popq _ | Negq _ | Callq _ | Retq -> live
 
-let f lives rinstrs =
-  let f' lives instr = g instr (List.hd lives) :: lives in
-  List.fold_left f' lives rinstrs
+let h lives rinstrs =
+  let h' lives instr = g instr (List.hd lives) :: lives in
+  List.fold_left h' lives rinstrs
 
-let uncover instrs = f [[]] (List.rev instrs)
+let uncover instrs = h [[]] (List.rev instrs)
+
+let process_named_block (label, (Block (_, instrs))) =
+  (label, Block (uncover instrs, instrs))
+
+
+let f = function Program (info, nbs) -> Program (info, List.map process_named_block nbs)
