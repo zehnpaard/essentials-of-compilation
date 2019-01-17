0ddbe3e024f57cdfa9d86729e77cf8d22921c49f
zehnpaard <zehnpaard@googlemail.com>
2019-01-17 22:11:46 +0900

    Add h_callq to build_interference

--- ch03/build_interference.ml
+++ ch03/build_interference.ml
@@ -5,6 +5,12 @@ let rec h_arith d = function
   | v :: vs when v = d -> h_arith d vs
   | v :: vs -> (if v < d then (Var v, Var d) else (Var d, Var v)) :: h_arith d vs
 
+let caller_regs = [Rax; Rdx; Rcx; Rsi; Rdi; R8; R9; R10; R11]
+
+let rec h_callq = function
+  | [] -> []
+  | v :: vs -> List.map (fun r -> (Var v, Reg r)) caller_regs @ h_callq vs
+
 let g res l_after = function
   | Addq (_, Var d) | Subq  (_, Var d) | Negq (Var d) -> h_arith d l_after :: res
   | Callq _ -> h_callq l_after :: res
