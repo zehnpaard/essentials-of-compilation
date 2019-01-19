8f93a80697296242b0e0d5f336da318215e3f73a
zehnpaard <zehnpaard@googlemail.com>
2019-01-20 01:57:09 +0900

    Use build_interference and color_graph in main

--- ch03/main.ml
+++ ch03/main.ml
@@ -9,6 +9,8 @@ let czp2 = Uncover.uncover_locals czp1
 let Czero.Program (info, _) = czp2
 let asmp1 = Select_instructions.f czp2
 let asmp2 = Uncover_live.f asmp1
+let asmp2 = Build_interference.f asmp2
+let asmp2 = Color_graph.f asmp2
 let asmp3 = Assign_home.f asmp2
 let asmp4 = Patch_instructions.f asmp3
 
