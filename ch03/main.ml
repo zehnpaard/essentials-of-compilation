12c6a681154ef8281e0a4fa382a9a1ffb9e0ac99
zehnpaard <zehnpaard@googlemail.com>
2019-01-20 18:54:03 +0900

    Use List_vars in main

--- ch03/main.ml
+++ ch03/main.ml
@@ -5,13 +5,12 @@ let astp = Program ([], x)
 let astp2 = Uniquify.f astp
 let astp3 = Decomplex.f astp2
 let czp1 = Explicate.f astp3
-let czp2 = Uncover.uncover_locals czp1
-let Czero.Program (info, _) = czp2
-let asmp1 = Select_instructions.f czp2
+let asmp1 = Select_instructions.f czp1
 let asmp2 = Uncover_live.f asmp1
 let asmp2 = Build_interference.f asmp2
 let asmp2 = Color_graph.f asmp2
 let asmp2 = Allocate_reg.f asmp2
+let asmp2 = List_vars.f asmp2
 let asmp3 = Assign_home.f asmp2
 let asmp4 = Patch_instructions.f asmp3
 
@@ -21,7 +20,6 @@ let () =
     print_endline (string_of_prog astp2);
     print_endline (string_of_prog astp3);
     print_endline (Czero.string_of_prog czp1);
-    print_endline (Czero.string_of_prog czp2);
     print_endline (Asm.string_of_prog asmp1);
     print_endline (Asm.string_of_prog asmp2);
     print_endline (Asm.string_of_prog asmp3);
