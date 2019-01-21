72c33219df9445ffbcab0771c99f3112edcf69d9
zehnpaard <zehnpaard@googlemail.com>
2019-01-22 05:58:39 +0900

    Fix bug in uncover_live

--- ch03/uncover_live.ml
+++ ch03/uncover_live.ml
@@ -4,9 +4,9 @@ let g instr live = match instr with
   | Addq (a1, a2) | Subq (a1, a2) | Movq (a1, a2) ->
     (match a1, a2 with
      | Var v1, Var v2 when v1 = v2 -> live
-     | Var v1, Var v2 -> v1 :: List.filter ((=) v2) live
+     | Var v1, Var v2 -> v1 :: List.filter ((<>) v2) live
      | Var v, _ -> v :: live
-     | _, Var v -> List.filter ((=) v) live
+     | _, Var v -> List.filter ((<>) v) live
      | _, _ -> live)
   | Pushq (Var v) -> v :: live
   | Popq (Var v) -> List.filter ((=) v) live
