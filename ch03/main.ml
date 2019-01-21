cc35e402b41d32312aba056f254892675250daa5
zehnpaard <zehnpaard@googlemail.com>
2019-01-21 23:06:48 +0900

    Modify main to have separate variables for each step

--- ch03/main.ml
+++ ch03/main.ml
@@ -7,19 +7,17 @@ let astp3 = Decomplex.f astp2
 let czp1 = Explicate.f astp3
 let asmp1 = Select_instructions.f czp1
 let asmp2 = Uncover_live.f asmp1
-let asmp2 = Build_interference.f asmp2
-let asmp2 = Color_graph.f asmp2
-let asmp2 = Allocate_reg.f asmp2
-let asmp2 = List_vars.f asmp2
-let asmp3 = Assign_home.f asmp2
-let asmp4 = Patch_instructions.f asmp3
+let asmp3 = Build_interference.f asmp2
+let asmp4 = Color_graph.f asmp3
+let asmp5 = Allocate_reg.f asmp4
+let asmp6 = List_vars.f asmp5
+let asmp7 = Assign_home.f asmp6
+let asmp8 = Patch_instructions.f asmp7
 
 let () = 
   begin
-    print_endline (Asm.string_of_prog asmp1);
-    print_endline (Asm.string_of_prog_block_info asmp1);
     print_endline (Asm.string_of_prog asmp4);
     print_endline (Asm.string_of_prog_block_info asmp4);
-    print_endline (Asm.string_of_prog (Compile.f astp));
-    print_endline (Asm.string_of_prog_block_info (Compile.f astp));
+    print_endline (Asm.string_of_prog asmp8);
+    print_endline (Asm.string_of_prog_block_info asmp8);
   end
